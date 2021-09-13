7ee8c8c249db15e6b881bbff4f1b59bdcab90cd6
likawind <ubuntu@ip-172-31-45-190.us-east-2.compute.internal>
2021-09-13 17:58:08 +0000

    initial commit

---
+++ model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+class Function:
+    def predict(self, df):
+        # Inputs:
+        # df: pd.DataFrame:
+        #   A dataframe representing the inputs.
+        #
+        # Returns: pd.DataFrame:
+        #   a new dataframe which will be captured in output table
+
+        return df
