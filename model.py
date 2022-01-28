abc82987b6d7f4e751c0a6bde4bb0cc344f36dda
likawind <ubuntu@ip-172-31-45-190.us-east-2.compute.internal>
2022-01-28 20:19:28 +0000

    update

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 class Function:
-    def predict(self, df):
+    def predict(self, df, wine):
         # Inputs:
         # df: pd.DataFrame:
         #   A dataframe representing the inputs.
@@ -8,4 +8,5 @@ class Function:
         # Returns: pd.DataFrame:
         #   a new dataframe which will be captured in output table
         df['new'] = df['review'].str.upper()
+        df['color'] = wine['color'].iloc[0]
         return df
