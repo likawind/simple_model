c1357af91ef22e9396e68eb8c36af7ce6e90da1c
likawind <ubuntu@ip-172-31-45-190.us-east-2.compute.internal>
2022-02-01 22:58:43 +0000

    add impl

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 import pandas as pd
 class Function:
-    def predict(self):
+    def predict(self, df):
         # Inputs:
         # df: pd.DataFrame:
         #   A dataframe representing the inputs.
         #
         # Returns: pd.DataFrame:
         #   a new dataframe which will be captured in output table
-        return pd.DataFrame.from_dict({'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']})
+        print(df)
+        return pd.Series([df.shape[0] == 2])
+        #return pd.DataFrame.from_dict({'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']})
