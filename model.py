e3328622bfaac86c54f39a9ed1341a29867b67e3
likawind <ubuntu@ip-172-31-45-190.us-east-2.compute.internal>
2022-02-02 01:09:31 +0000

    fix num

--- model.py
+++ model.py
@@ -8,5 +8,5 @@ class Function:
         # Returns: pd.DataFrame:
         #   a new dataframe which will be captured in output table
         print(df)
-        return pd.Series([df.shape[0] == 2])
+        return pd.Series([df.shape[0] == 4])
         #return pd.DataFrame.from_dict({'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']})
