3c3bf8b5ce6ff913472f2fa421c4244f904cfa6c
likawind <ubuntu@ip-172-31-45-190.us-east-2.compute.internal>
2022-03-10 06:12:48 +0000

    fix model

--- models/copy_review/model.py
+++ models/copy_review/model.py
@@ -1,12 +1,5 @@
 import pandas as pd
 class Function:
     def predict(self, df):
-        # Inputs:
-        # df: pd.DataFrame:
-        #   A dataframe representing the inputs.
-        #
-        # Returns: pd.DataFrame:
-        #   a new dataframe which will be captured in output table
-        print(df)
-        return pd.Series([df.shape[0] == 4])
-        #return pd.DataFrame.from_dict({'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']})
+        df['new'] = df['review']
+        return df
